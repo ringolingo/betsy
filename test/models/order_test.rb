663ecbc6c0dd46a863e044701928e1f6f18990f2
Iris Lux <iris.lux0@gmail.com>
2020-11-17 17:20:52 -0800

    added blank tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 describe Order do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  before do
+    @order = orders(:betty)
+  end
+
+  describe 'validations' do
+
+    it 'is valid when all fields are present' do
+      expect(@order.valid?).must_equal true
+    end
+
+  end
+
+  describe 'relationships' do
+
+    describe 'will be invalid without OrderItem' do
+
+    end
+
+    describe 'can add an OrderItem' do
+
+  end
 end
