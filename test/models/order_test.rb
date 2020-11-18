9f61f418c9f7652bd93cf04dfa1c9bf837078f9d
Iris Lux <iris.lux0@gmail.com>
2020-11-17 19:57:34 -0800

    Order tests for validations, Order validations, changed ccnum from int to bigint

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -9,9 +9,27 @@ describe Order do
   describe 'validations' do
 
     it 'is valid when all fields are present' do
+      #order_item = OrderItem.create(product_id: 1, order_id: @order.id)
       expect(@order.valid?).must_equal true
     end
 
+    it 'will be invalid without status' do
+
+      @order.status = nil
+
+      expect(@order.valid?).must_equal false
+      expect(@order.errors.messages).must_include :status
+      expect(@order.errors.messages[:status][0]).must_equal "can't be blank"
+
+    end
+
+    it 'will be invalid with status other than paid or processing' do
+      @order.status = 'nope'
+
+      expect(@order.valid?).must_equal false
+      expect(@order.errors.messages).must_include :status
+      expect(@order.errors.messages[:status][0]).must_equal "nope is not a valid status"
+    end
   end
 
   describe 'relationships' do
@@ -20,7 +38,14 @@ describe Order do
 
     end
 
-    describe 'can add an OrderItem' do
+    it 'OrderItem can add an Order' do
+      order_item = OrderItem.create(product_id: 1, order_id: @order.id)
+      expect(order_item.order.name).must_equal "Betty Elms"
+    end
+
+    describe 'Can access Product through OrderItem' do
+
+    end
 
   end
 end
