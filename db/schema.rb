f00eda7ec89c024164b78bcc2c0eb6068c45dda8
Iris Lux <iris.lux0@gmail.com>
2020-11-18 15:27:56 -0800

    added relationships to Order and assoc. tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_035430) do
+ActiveRecord::Schema.define(version: 2020_11_18_230651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,13 @@ ActiveRecord::Schema.define(version: 2020_11_18_035430) do
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "order_id"
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "product_id"
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -48,7 +50,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_035430) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "category"
     t.string "description"
     t.integer "price"
     t.string "photo_url"
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_035430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "merchant_id"
+    t.string "category"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
