0b81a8c5445e6df1ec4eacc5b3f21a097bf0b0c2
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-17 17:16:51 -0800

    add merchant validation tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_17_232947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 describe Merchant do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
+  describe "validations" do
+    before do
+      @new_merchant = Merchant.new(username: "merchant", email: "isell@stuff.com")
+    end
+
+    it "creates a merchant given a username and email" do
+      @new_merchant.save
+
+      expect(@new_merchant.valid?).must_equal true
+    end
+
+    it "does not create a new merchant without a username" do
+      @new_merchant.username = nil
+
+      expect(@new_merchant.valid?).must_equal false
+    end
+
+    it "does not create a new merchant without an email" do
+      @new_merchant.email = nil
+
+      expect(@new_merchant.valid?).must_equal false
+    end
+
+    it "does not create a new merchant with a duplicate username" do
+      @new_merchant.save
+
+      @repeat = Merchant.new(username: "merchant", email: "ialsosell@stuff.com")
+
+      expect(@repeat.valid?).must_equal false
+    end
+
+    it "does not create a new merchant with a duplicate email" do
+      @new_merchant.save
+
+      @repeat = Merchant.new(username: "another merchant", email: "isell@stuff.com")
+
+      expect(@repeat.valid?).must_equal false
+    end
+  end
+
+  # describe "relationships" do
+  #
   # end
 end
