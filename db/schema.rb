e2ac79dfb2aa45d163153c22c85d44eac83cf348
Sophia Donan <sophiadonan@gmail.com>
2020-11-18 06:39:44 -0800

    updated schema, updated product model, created product model test

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_11_17_230644) do
-
+ActiveRecord::Schema.define(version: 2020_11_18_135953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "orders", force: :cascade do |t|
     t.string "status"
@@ -33,5 +30,16 @@ ActiveRecord::Schema.define(version: 2020_11_17_230644) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "description"
+    t.integer "price"
+    t.string "photo_url"
+    t.integer "stock"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+
+  end
 
 end
