00bded6b8155d292c2fc6440306157a4a3114f85
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-18 20:30:30 -0800

    build merchant show view and add order history table

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,4 +2,13 @@ class MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
   end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+
+    if @merchant.nil?
+      flash[:error] = "No such person"
+      redirect_to merchants_path and return
+    end
+  end
 end
