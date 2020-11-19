bdab1b9df822296b10c8de660dc3d860bba67165
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-18 16:51:33 -0800

    create merchants_orders join table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_items
+  has_and_belongs_to_many :merchants
   validates :status, presence: true, inclusion: { in: %w(pending paid),
                message: "%{value} is not a valid status" }
 end
