8b112d4b6a82c52fafca56e6fd08bdae718b7452
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-11-18 17:40:35 -0800

    added create Product for user in session + some partial views for products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_035430) do
+ActiveRecord::Schema.define(version: 2020_11_18_135953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_035430) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "category"
     t.string "description"
     t.integer "price"
     t.string "photo_url"
@@ -56,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_035430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "merchant_id"
+    t.string "category"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
