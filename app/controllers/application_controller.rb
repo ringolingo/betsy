c3a608222e13d3583a0fada6279ea58904fc7e52
Iris Lux <iris.lux0@gmail.com>
2020-11-18 21:15:17 -0800

    OrderItem create and respective test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+before_action :set_current_order
 
 private
 
   def set_current_order
-    @current_user = Order.find_by(id: session[:order_id])
+    @current_order = Order.find_by(id: session[:order_id])
   end
 
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,27 +4,43 @@ class OrderItemsController < ApplicationController
 
   def create
 
-    product = Product.find_by(id: params[:id])
+    product = Product.find_by(id: params[:product_id])
 
-    unless @order
-      order = Order.create(status: "pending")
+    save_order = true
+
+    if @current_order.nil?
+
+      order = Order.new(status: "pending")
+
+      save_order = order.save
 
       session[:order_id] = order.id
 
-
+      set_current_order
     end
 
+    order_item = OrderItem.new(order: @current_order, product: product, quantity: params[:order_item][:quantity])
+
+    save_order_item = order_item.save!
+
+    if(save_order && save_order_item)
+      flash[:success] = "Item added to cart!"
+      redirect_to orders_path
+    else
+      flash[:error] = "Unable to add item"
+      render "orders/index", status: :bad_request
+    end
   end
 
   def destroy
     redirect_to orders_path and return if @order_item.nil?
 
-    if @order_item.update(order_item_params)
+    if @order_item.destroy
       flash[:success] = "Order item successfully deleted"
       redirect_to orders_path
     else
       flash.now[:error] = "Error: unable to delete orders item"
-      render orders_path, status: :bad_request
+      render "orders/index", status: :bad_request
     end
 
   end
@@ -39,7 +55,8 @@ class OrderItemsController < ApplicationController
     else
       flash.now[:error] = "Error: unable to update orders item"
       redirect_to orders_path
-      #render orders_url, status: :bad_request
+      render "orders/index", status: :bad_request
+      #render "order/#{@current_order.id}/show", status: :bad_request
     end
   end
 
