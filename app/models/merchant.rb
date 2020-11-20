c8ad6eaf6c91c90e5c0f76a97c8bc928d307ba1d
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-19 20:12:49 -0800

    minor error fixes and draft product controller tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :uid, uniqueness: {scope: :provider}
-  validates :username, presence: true
+  # validates :username, presence: true
 
   def self.build_from_github(auth_hash)
     merchant = Merchant.new
