5a963127a6d56d3faf9e3ebdb3ad785a7f2293ad
Sophia Donan <sophiadonan@gmail.com>
2020-11-19 17:58:05 -0800

    Created Homepage controller/view, created logout method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'homepages#index'
+
   #OmniAuth Login Route
   get "/auth/github", as: "github_login"
-
   #OmniAuth Github callback route
-  get "auth/:provider/callback", to: "merchant#create", as: "omniauth_callback"
+  get "auth/:provider/callback", to: "merchants#create", as: "omniauth_callback"
+  delete "/logout", to: "merchants#logout", as: "logout"
 
   resources :products do
     resources :order_items, only: [:create]
