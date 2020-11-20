7eb0723b34dd8a1babe820df0797dce44fe92040
Sophia Donan <sophiadonan@gmail.com>
2020-11-20 01:45:20 -0800

    Created a navigation that changes based on logged in/out user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_11_20_051550) do
-=======
-ActiveRecord::Schema.define(version: 2020_11_20_004646) do
->>>>>>> f4af768... possible unstaged changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
