2b4653346b0c7810fdf12425a2fb47fedea41059
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-19 21:20:21 -0800

    migrate remove total/subtotal from order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_004646) do
+ActiveRecord::Schema.define(version: 2020_11_20_051550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,6 @@ ActiveRecord::Schema.define(version: 2020_11_20_004646) do
 
   create_table "orders", force: :cascade do |t|
     t.string "status"
-    t.integer "sub_total"
-    t.integer "order_total"
     t.string "name"
     t.string "address"
     t.string "email"
