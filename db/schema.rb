e5f191cdb25ac4a0eac46c1b89d3c99f0db5f7be
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-19 17:19:25 -0800

    add for_sale status to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_164012) do
+ActiveRecord::Schema.define(version: 2020_11_20_004646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_164012) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "merchant_id"
     t.string "category"
+    t.boolean "for_sale"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
