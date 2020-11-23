c3f07dd0d63e96d120bfa63588ed85a0ae2558f3
Iris Lux <iris.lux0@gmail.com>
2020-11-22 17:06:45 -0800

    model validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,8 @@ class Order < ApplicationRecord
   has_many :products, through: :order_items
   validates :status, presence: true, inclusion: { in: %w(pending paid),
                message: "%{value} is not a valid status" }
+  validates :name, presence: true
+ 
 
   def return_merchants
     merchants = []
