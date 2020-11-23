6fa93099c9c943873273621e6934dce0b10d446f
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-22 16:00:06 -0800

    write products controller tests show, index, toggle_for_sale

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,26 +2,74 @@ require "test_helper"
 
 describe ProductsController do
   describe "index" do
-    it "returns a variable with all products" do
-      raise NotImplementedError
+    it "responds with success when there are many products" do
+      get products_path
+
+      must_respond_with :success
     end
 
-    it "returns a variable with only active products" do
-      raise NotImplementedError
+    it "responds with success when there are no products" do
+      Product.destroy_all
+
+      get products_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "responds with success for a valid product" do
+      product = products(:product1)
+
+      get product_path(product.id)
+
+      must_respond_with :success
+    end
+
+    it "redirects for an invalid product" do
+      product_id = 800
+
+      get product_path(product_id)
+
+      must_respond_with :redirect
     end
   end
 
   describe "toggle_for_sale" do
     it "retires an active product" do
-      raise NotImplementedError
+      merchant = merchants(:merchant1)
+      product = products(:product1)
+      perform_login(merchant)
+      status = product.for_sale
+
+      patch toggle_for_sale_path(product)
+
+      expect(product.for_sale).must_equal !status
+      must_respond_with :redirect
     end
 
     it "makes a retired product active again" do
-      raise NotImplementedError
+      merchant = merchants(:merchant1)
+      product = products(:product3)
+      perform_login(merchant)
+      status = product.for_sale
+
+      patch toggle_for_sale_path(product)
+
+      expect(product.for_sale).must_equal !status
+      must_respond_with :redirect
     end
 
     it "makes no change if user is not the product's seller" do
-      raise NotImplementedError
+      merchant = merchants(:merchant3)
+      product = products(:product1)
+      perform_login(merchant)
+      status = product.for_sale
+
+      patch toggle_for_sale_path(product)
+
+      must_respond_with :redirect
+      expect(product.for_sale).must_equal status
     end
   end
 end
