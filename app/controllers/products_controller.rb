9ad847093d3d1a5f7e7efeb464b706708dc1cb46
Sophia Donan <sophiadonan@gmail.com>
2020-11-23 16:06:01 -0800

    accidentily put category under private products controller, moved up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -91,6 +91,15 @@ class ProductsController < ApplicationController
     redirect_to merchant_path(@merchant) and return
   end
 
+  def category
+    @category = Category.find_by(id: params[:id])
+    if @category.nil?
+      flash[:warning] = "Category is invalid"
+      redirect_to root_path
+    end
+    @products = Product.by_category(params[:id])
+  end
+
   private
 
   def product_params
@@ -101,13 +110,5 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
   end
 
-  def category
-    @category = Category.find_by(id: params[:id])
-    if @category.nil?
-      flash[:warning] = "Category is invalid"
-      redirect_to root_path
-    end
-    @products = Product.by_category(params[:id])
-  end
 
 end
