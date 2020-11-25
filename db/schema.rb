27b89386bf7a48e7f0ed9d57ff3396b7ec649c98
Sophia Donan <sophiadonan@gmail.com>
2020-11-24 20:37:58 -0800

    reviews table now shows without error on show pageyarn install --check-files a y

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_011827) do
+ActiveRecord::Schema.define(version: 2020_11_25_042317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,8 +105,11 @@ ActiveRecord::Schema.define(version: 2020_11_25_011827) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "product_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
