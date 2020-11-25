53fe81982eb6057e57ddbb4f8974ce427aa052a1
Sophia Donan <sophiadonan@gmail.com>
2020-11-25 09:30:08 -0800

    started over reviews controller test

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -4,28 +4,18 @@ require "test_helper"
 
     describe "new" do
       it "gets a from for a new product review page" do
-        # get new_product_review_path(products(:product1).id)
-        #
-        # must_equal :success
+
       end
     end
 
     describe "create" do
-      # before do
-      #   @review = {review: {rating: 3, review: "a very solid review"} }
-      # end
+
 
       it "creates a new review" do
-        # expect{
-        #   post product_reviews_path(products(:product1).id), params: @review
-        # }.must_change "Review.count", 1
-        #
-        # must_redirect_to product_path(products(:product1).id)
-        # new_review = Review.find_by(product_id: products(:product1).id, review: "a very solid review")
-        # expect(new_review.rating).must_equal @review[:review][:rating]
+
       end
 
-      it "doesn't create a review for a product that doesn't exist" do
+      it "does not create a review for a product that is not on site" do
 
       end
     end
