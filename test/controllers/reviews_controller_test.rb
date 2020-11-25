b40475ea6ada044888a7fb4857204b5386f54458
Sophia Donan <sophiadonan@gmail.com>
2020-11-24 21:45:05 -0800

    made outline for reviews controller

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
+  describe ReviewsController do
 
-describe ReviewsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
-end
+
+    it "gets index" do
+
+    end
+
+    describe "new" do
+
+    end
+
+    describe "create" do
+
+      it "creates a new review given valid params" do
+
+      end
+
+      it "respond with an redirect for invalid params" do
+
+      end
+
+      it "respond with an error if merchant reviews their own product" do
+
+      end
+
+    end
+
+
+  end
