2f0d6ee872b24517a45828b662aa04e8820efcd4
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-24 17:39:49 -0800

    remove category column from product and update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_095916) do
+ActiveRecord::Schema.define(version: 2020_11_25_005433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,7 +99,6 @@ ActiveRecord::Schema.define(version: 2020_11_24_095916) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "merchant_id"
-    t.string "category"
     t.boolean "for_sale"
     t.string "photo_url", default: "https://i.ibb.co/sHTKJ62/moon.jpg"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
