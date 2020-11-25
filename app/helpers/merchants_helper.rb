c3a3205ad20a08b9a47e1d8bc4f81fc6d47298f6
Iris Lux <iris.lux0@gmail.com>
2020-11-25 02:40:27 -0800

    jumbotron and vards for merchant show

--- app/helpers/merchants_helper.rb
+++ app/helpers/merchants_helper.rb
@@ -1,2 +1,10 @@
 module MerchantsHelper
+
+  def display_desc(desc)
+    return desc ? "#{desc}." : ""
+  end
+
+  def change_status_btn(for_sale)
+    return for_sale ? "Retire" : "Make Active"
+  end
 end
